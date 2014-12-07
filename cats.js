4e00737edbfc066c48993058dff17c6b2c2c87df
Aditya Kodkany <adityakodkany@gmail.com>
2014-12-07 09:05:22 +0000

    added cats and start pos for cats

---
+++ cats.js
@@ -0,0 +1,18 @@
+var catsJSON = {
+    cats: [
+        {
+            "name": "fluffy",
+            "id": 0,
+            "color": "77ff44",
+            "path": [{"x":1,"y":1},{"x":1,"y":2},{"x":2,"y":2}],
+            "pos": {"x":0, "y":0},
+        },
+        {
+            "name": "kitcat",
+            "id": 1,
+            "color": "ffff44",
+            "path": [{"x":2,"y":3},{"x":3,"y":3},{"x":3,"y":2}],
+            "pos": {"x":2, "y":2},
+        }
+    ]
+}
