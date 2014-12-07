15de3983084249adfec9300fa3d2996e11f2922a
Aditya Kodkany <adityakodkany@gmail.com>
2014-12-07 08:16:36 +0000

    draw map

---
+++ map.js
@@ -0,0 +1,138 @@
+var mapJSON = {
+    "map": [
+        [{
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        ],
+        [
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        ],
+        [
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        ],
+        [
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        ],
+        [
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        {
+            "tile_base": 0,
+            "lock": null,
+            "pen_ids": [],
+        },
+        ]
+    ]
+};
