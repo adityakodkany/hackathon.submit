c641e01001589c8471896373cd53b1a51ae85fd4
Matthew Kellogg <kellogg92@gmail.com>
2014-12-08 07:23:48 +0000

    added pens

--- map.js
+++ map.js
@@ -7,29 +7,29 @@ var mapJSON = {
     "cats": [
         {
             "name": "fluffy",
-            "color": 0x77ff44,
+            "color": {r: 128, g: 255, b: 64},
             "path": [{"x":0, "y":0},{"x":1,"y":0},{"x":1,"y":1},{"x":1,"y":2}]
         },
         {
             "name": "kitcat",
-            "color": 0xFFFF44,
+            "color":{r: 255, g: 255, b: 40},
             "path": [{"x":2, "y":2},{"x":2,"y":3},{"x":3,"y":3},{"x":3,"y":2}]
         }
     ],
     "pens": [
         {
-            "color": 0xCCDD11,
+            "color":{r: 230, g: 240, b: 17},
             "rects":[
                 {
-                    "rs": 0,
-                    "re": 2,
-                    "cs": 1,
-                    "ce": 1
+                    "rs": 2,
+                    "re": 4,
+                    "cs": 3,
+                    "ce": 6
                 }
             ]
         },
         {
-            "color": 0xAA2222,
+            "color":{r: 200, g: 50, b: 50},
             "rects":[
                 {
                     "rs": 0,
