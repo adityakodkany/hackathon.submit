74f68ea6c59c4669b9e68dea1054eae80faed1fd
Aditya Kodkany <adityakodkany@gmail.com>
2014-12-08 00:23:51 +0000

    begin locking on cat-step; fix map indexing in updateCats

--- map.js
+++ map.js
@@ -2,17 +2,15 @@ var mapJSON = {
     "map": [
         [{
             "tile_base": 0,
-            "path": 1,
             "lock": null,
             "pen_ids": [],
             "is_locked": false,
         },
         {
             "tile_base": 0,
-            "path": 3,
             "lock": null,
             "pen_ids": [],
-            "is_locked": false,
+            "is_locked": true,
         },
         {
             "tile_base": 0,
@@ -37,12 +35,11 @@ var mapJSON = {
         {
             "tile_base": 0,
             "lock": null,
-            "pen_ids": [0],
-            "is_locked": true,
+            "pen_ids": [],
+            "is_locked": false,
         },
         {
             "tile_base": 0,
-            "path": 0,
             "lock": null,
             "pen_ids": [],
             "is_locked": false,
